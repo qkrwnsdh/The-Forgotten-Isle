7d9f1bd585140a1be53c9ef26ca00b260509cb22
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2024-06-19 03:11:36 +0900

    feat: 네트워크 플레이어 움직임 동기화 기능 구현

--- The Forgotten Isle/Assets/TheForgottenIsle/Scripts/Player.cs
+++ The Forgotten Isle/Assets/TheForgottenIsle/Scripts/Player.cs
@@ -1,10 +1,10 @@
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Player : NetworkBehaviour
 {
     public NetworkVariable<Vector3> pos = new NetworkVariable<Vector3>();
+    private Vector3 lastServerPos;
 
     private void Update()
     {
@@ -12,32 +12,33 @@ public class Player : NetworkBehaviour
         {
             Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * 5.0f;
             transform.Translate(move);
-            pos.Value = transform.position;
 
-            if (!NetworkObject.IsOwner)
-            {
-                UpdatePositionServer(transform.position);
-            }
-            
+            // 클라이언트에서 즉시 보여주기 위해 예측 위치를 사용합니다.
+            Vector3 predictedPos = transform.position + move;
+
+            // 서버에 위치 업데이트를 요청합니다.
+            UpdatePositionServerRpc(predictedPos);
         }
         else
         {
-            transform.position = pos.Value;
+            // 서버에서 받은 최신 위치로 보간하여 부드러운 움직임을 만듭니다.
+            transform.position = Vector3.Lerp(transform.position, lastServerPos, Time.deltaTime * 10f);
         }
     }
 
     [ServerRpc]
-    private void UpdatePositionServer(Vector3 targetPos)
+    private void UpdatePositionServerRpc(Vector3 targetPos)
     {
-        if (IsServer)
-        {
-            UpdatePositionClient(targetPos);
-        }
+        pos.Value = targetPos;
+
+        // 모든 클라이언트에게 위치 업데이트를 알립니다.
+        UpdatePositionClientRpc(targetPos);
     }
 
     [ClientRpc]
-    private void UpdatePositionClient(Vector3 targetPos)
+    private void UpdatePositionClientRpc(Vector3 targetPos)
     {
-        transform.position = targetPos;
+        // 클라이언트는 서버에서 받은 위치를 최신화합니다.
+        lastServerPos = targetPos;
     }
 }
