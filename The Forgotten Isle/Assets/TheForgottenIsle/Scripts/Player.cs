73ff9ee5982d88035a6d10b1513c8201da0ca1e8
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2024-06-19 02:39:38 +0900

    other: 네트워크 테스트

--- The Forgotten Isle/Assets/TheForgottenIsle/Scripts/Player.cs
+++ The Forgotten Isle/Assets/TheForgottenIsle/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine.UIElements;
 
 public class Player : NetworkBehaviour
 {
-    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    public NetworkVariable<Vector3> pos = new NetworkVariable<Vector3>();
 
     private void Update()
     {
@@ -12,11 +12,32 @@ public class Player : NetworkBehaviour
         {
             Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * 5.0f;
             transform.Translate(move);
-            Position.Value = transform.position;
+            pos.Value = transform.position;
+
+            if (!NetworkObject.IsOwner)
+            {
+                UpdatePositionServer(transform.position);
+            }
+            
         }
         else
         {
-            transform.position = Position.Value;
+            transform.position = pos.Value;
         }
+    }
+
+    [ServerRpc]
+    private void UpdatePositionServer(Vector3 targetPos)
+    {
+        if (IsServer)
+        {
+            UpdatePositionClient(targetPos);
+        }
+    }
+
+    [ClientRpc]
+    private void UpdatePositionClient(Vector3 targetPos)
+    {
+        transform.position = targetPos;
     }
 }
