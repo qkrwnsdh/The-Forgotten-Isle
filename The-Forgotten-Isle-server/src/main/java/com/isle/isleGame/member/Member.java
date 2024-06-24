a1c4db628708130fd24c9d5388771ddb83f314ba
yjin-desktop <yjinlee99@gmail.com>
2024-06-24 17:51:18 +0900

    add: 회원가입 테스트 추가

--- The-Forgotten-Isle-server/src/main/java/com/isle/isleGame/member/Member.java
+++ The-Forgotten-Isle-server/src/main/java/com/isle/isleGame/member/Member.java
@@ -3,10 +3,12 @@ package com.isle.isleGame.member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Data
+@Entity @Data
+@AllArgsConstructor @NoArgsConstructor
 public class Member {
 
     @Id @Column(name = "member_id")
--- The-Forgotten-Isle-server/src/test/java/com/isle/isleGame/member/MemberServiceTest.java
+++ The-Forgotten-Isle-server/src/test/java/com/isle/isleGame/member/MemberServiceTest.java
@@ -3,8 +3,10 @@ package com.isle.isleGame.member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MemberServiceTest {
@@ -13,10 +15,26 @@ class MemberServiceTest {
     MemberService memberService;
 
     @Test
-    void 비밀번호_인코딩_확인() {
+    void 회원가입_성공() {
+        JoinDTO dto = new JoinDTO();
+        dto.setId("qwe");
+        dto.setPassword("1234");
 
+        ResponseEntity<Object> entity = memberService.join(dto);
+
+        assertEquals(entity.getStatusCode(), HttpStatus.OK);
     }
+
     @Test
-    void join() {
+    void 회원가입_실패() {
+        JoinDTO dto = new JoinDTO();
+        dto.setId("qwe");
+        dto.setPassword("1234");
+
+        memberService.join(dto);
+
+        ResponseEntity<Object> entity = memberService.join(dto);
+
+        assertEquals(entity.getStatusCode(), HttpStatus.NOT_FOUND);
     }
 }
